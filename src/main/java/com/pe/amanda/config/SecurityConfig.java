a5c6fdeae4caa55c67c0eded8165e1e80a0bcc07
RefinedCandle49 <valdezvargaskevin@gmail.com>
2023-12-29 08:00:14 -0500

    Remitente funcionando pero no correctamente, me pone la un dato adicional el cual es 'envíos'

--- src/main/java/com/pe/amanda/config/SecurityConfig.java
+++ src/main/java/com/pe/amanda/config/SecurityConfig.java
@@ -11,10 +11,12 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -39,6 +41,11 @@ public class SecurityConfig {
                 .requestMatchers("/remitente/listar")
                 .authenticated()
                 .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
