6502bff84bdd1bf10fb7ea9bfd0432b8d3150e56
RefinedCandle49 <valdezvargaskevin@gmail.com>
2023-12-30 17:48:38 -0500

    Borrado de la columna email para la tabla "userinfo" y agregado de unique al valor name para que no se repita en la tabla

--- src/main/java/com/pe/amanda/credenciales/UserInfo.java
+++ src/main/java/com/pe/amanda/credenciales/UserInfo.java
@@ -14,8 +14,8 @@ public class UserInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(unique=true)
     private String name;
-    private String email;
     private String password;
     private String roles;
 }
