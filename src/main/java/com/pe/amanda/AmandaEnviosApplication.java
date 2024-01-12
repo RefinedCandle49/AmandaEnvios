c4a131f67cec7c8fd0c3257ea3d179db554dc938
RefinedCandle49 <valdezvargaskevin@gmail.com>
2024-01-12 09:18:56 -0500

    Se añadió Swagger usando Spring Doc Open API

--- src/main/java/com/pe/amanda/AmandaEnviosApplication.java
+++ src/main/java/com/pe/amanda/AmandaEnviosApplication.java
@@ -1,16 +1,32 @@
 package com.pe.amanda;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Library APIS", version = "1.0", description = "Library Management Apis."))
 public class AmandaEnviosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AmandaEnviosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AmandaEnviosApplication.class, args);
+    }
 
+    @Bean
+    public OpenAPI springAmandaEnviosAPI() {
+        return new OpenAPI()
+                .info(new Info().title("API REST Amanda Envios")
+                        .termsOfService("Terms of service")
+                        .description("API's Rest de Amanda Envios, para clientes front-end")
+                        .version("1.0")
+                        .license(new License().name("All rights reserved").url("www.amandaenvios.pe")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Amanda Envios Documentation")
+                        .url("www.amandaenvios.pe/docs"));
+
+    }
 }
